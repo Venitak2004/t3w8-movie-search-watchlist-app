8374e4add4813d1b7465928a79a0d905167bd368
Venita Kissell <venitak@hotmail.com>
2025-01-15 21:54:57 +1000

    feat: fetched the movies from the api based on the title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,50 @@
+import axios from "axios";
 import React, { useState } from "react";
 
-export default function Home(){
-    const [query, setQuery] = useState('');
+export default function Home() {
+  const [query, setQuery] = useState("");
+  const [movies, setmovies] = useState([]);
 
-    const fetchMovies = async() => {
-        // Using fetch
-        //const response = await fetch(`https://www.omdbapi.com/${apikey}/${query}`);
-    }
+  const fetchMovies = async () => {
+    // Using fetch
+    //const response = await fetch(`https://www.omdbapi.com/${apikey}/${query}`);
 
-    return (
-        <div>
-            <h1>Move Search</h1>
-            <input
-            type= "text"
-            placeholder="Search for Movies..."
-            />
-            <button>Search</button>
-        </div>
-    )
+    // Using axios
+    if (query) {
+      const response = await axios.get(`https://www.omdbapi.com/`, {
+        params: {
+          apikey: "1db4bca0",
+          s: query
+        }
+      });
+      console.log(response.data);
+      setmovies(response.data.Search || []);
     }
+  };
+
+  return (
+    <div>
+      <h1>Move Search</h1>
+      <input
+        type="text"
+        placeholder="Search for Movies..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button onclick={fetchMovies}>Search</button>
+
+      <div>
+        {
+            movies.map((movie) => (
+                <div key={movie.imdbID}>
+                    <h3>{movie.Title}</h3>
+                    <p>{movie.Year}</p>
+                    </div>
+
+            ))
+        }
+      </div>
+
+    </div>
+  );
+}
